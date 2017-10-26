55fa72dd19b0e1626141ebf10369e9dd78c1f0c0
Brian Cribb <brian.cribb@gmail.com>
2017-10-26 14:02:53 -0400

    More tinkering.

--- vue/src/scripts/wa.js
+++ vue/src/scripts/wa.js
@@ -14,7 +14,59 @@ WA.init = function() {
 WA.vm = function (data) {
 	console.log(['vm: ', data]);
 
+data.message = "Hello, World!";
+
+
+Vue.component('planet-row', {
+	props: ['name', 'system', 'counter'],
+	data: function () {
+		return {
+			name: "Default Name"
+		}
+	},
+	template: `
+	<tr>
+		<td>{{ counter }}</td>
+		<td>{{ name }}</td>
+		<td>{{ system }}</td>
+	</tr>
+	`
+});
+
+var vm = new Vue({
+	el: '#app',
+	data :data,
+	methods : {
+		getSystemID : function(name) {
+			var that = this;
+			return _.find(that.systems, function(item){ return item.name === name; }).id;
+		}
+	},
+	template:`
+		<table class="table table-dark table-striped table-bordered table-responsive-sm">
+			<thead>
+				<tr>
+					<th scope="col"></th>
+					<th scope="col">Planet</th>
+					<th scope="col">System</th>
+				</tr>
+			</thead>
+			<tbody>
+			<tr v-for="(planet, index) in planets">
+				<td>{{ index }}</td>
+				<td>({{planet.id}}) {{ planet.name }}</td>
+				<td>({{ getSystemID(planet.system) }}) {{ planet.system }}</td>
+			</tr>
+			</tbody>
+		</table>
+		`
+});
+
+
+
+	$("#app").show();
 };
 
 
 
+// <planet-row v-bind:counter="index+1" v-bind:name="planet.name" v-bind:system="planet.system"></planet-row>
