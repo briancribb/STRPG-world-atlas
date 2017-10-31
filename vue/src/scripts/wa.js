2c10c67c4c6bbd041c0bc0e9b11a3a367dfd114a
Brian Cribb <brian.cribb@gmail.com>
2017-10-31 13:39:04 -0400

    Playing with methods.

--- vue/src/scripts/wa.js
+++ vue/src/scripts/wa.js
@@ -15,7 +15,7 @@ WA.vm = function (data) {
 	console.log(['vm: ', data]);
 
 data.message = "Hello, World!";
-
+data.orderBy = "system";
 
 Vue.component('planet-row', {
 	props: ['name', 'system', 'counter'],
@@ -37,28 +37,51 @@ var vm = new Vue({
 	el: '#app',
 	data :data,
 	methods : {
+		orderPlanets : function(orderBy) {
+			that = this;
+			if (orderBy) {
+				that.planets = _.sortBy(that.planets, function(planet){ return planet[orderBy] });
+			}
+		},
+		reverseArray : function(myArray) {
+			myArray = myArray.reverse();
+		},
 		getSystemID : function(name) {
 			var that = this;
 			return _.find(that.systems, function(item){ return item.name === name; }).id;
 		}
 	},
 	template:`
-		<table class="table table-dark table-striped table-bordered table-responsive-sm">
-			<thead>
-				<tr>
-					<th scope="col"></th>
-					<th scope="col">Planet</th>
-					<th scope="col">System</th>
+		<div class="stuff-and-things">
+			<ul class="nav-dark nav justify-content-center bg-dark text-white">
+				<li class="nav-item">
+					<a class="nav-link active" href="#" v-on:click.stop.prevent="orderPlanets('id')">By ID</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#" v-on:click.stop.prevent="orderPlanets('name')">A-Z</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#" v-on:click.stop.prevent="reverseArray(planets)">Reverse</a>
+				</li>
+			</ul>
+
+			<table class="table table-dark table-striped table-bordered table-responsive-sm">
+				<thead>
+					<tr>
+						<th scope="col"></th>
+						<th scope="col">Planet</th>
+						<th scope="col">System</th>
+					</tr>
+				</thead>
+				<tbody>
+				<tr v-for="(planet, index) in planets">
+					<th scope="row">{{ index }}</th>
+					<td>({{planet.id}}) {{ planet.name }}</td>
+					<td>({{ getSystemID(planet.system) }}) {{ planet.system }}</td>
 				</tr>
-			</thead>
-			<tbody>
-			<tr v-for="(planet, index) in planets">
-				<td>{{ index }}</td>
-				<td>({{planet.id}}) {{ planet.name }}</td>
-				<td>({{ getSystemID(planet.system) }}) {{ planet.system }}</td>
-			</tr>
-			</tbody>
-		</table>
+				</tbody>
+			</table>
+		</div>
 		`
 });
 	$("#app").show();
@@ -67,3 +90,25 @@ var vm = new Vue({
 
 
 // <planet-row v-bind:counter="index+1" v-bind:name="planet.name" v-bind:system="planet.system"></planet-row>
+
+
+/*
+<table class="table table-dark">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First Name</th>
+      <th scope="col">Last Name</th>
+      <th scope="col">Username</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+  </tbody>
+</table>
+*/
